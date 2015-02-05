42fa37418ea265d64a8298b580ac68852e812587
Mr21 <thomastortorini@gmail.com>
2015-02-05 18:58:47 +0100

    JS: btngrp: callback

--- css-reset.js
+++ css-reset.js
@@ -1,17 +1,18 @@
 $(function() {
 
 	function btngrp() {
-		var	jq_a = $(this).children(),
+		var	btnThis = this,
+			jq_a = $(this).children(),
 			jq_oldA = jq_a.filter(".css-selected");
-		jq_a.click(function(e) {
+		jq_a.click(function() {
 			var jq_this = $(this);
-			if (jq_this.hasClass("css-selected")) {
-				e.stopImmediatePropagation();
-				return false;
+			if (!jq_this.hasClass("css-selected")) {
+				jq_oldA.removeClass("css-selected");
+				jq_this.addClass("css-selected");
+				jq_oldA = jq_this;
+				if (btnThis.cssBtngrpCallback)
+					btnThis.cssBtngrpCallback.call(jq_this);
 			}
-			jq_oldA.removeClass("css-selected");
-			jq_this.addClass("css-selected");
-			jq_oldA = jq_this;
 		});
 		if (!jq_oldA[0])
 			jq_a.eq(0).click();
