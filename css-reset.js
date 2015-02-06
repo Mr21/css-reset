6cb5b220ebbc44f9d81440e9a044291538e77a86
Mr21 <thomastortorini@gmail.com>
2015-02-06 21:09:10 +0100

    JS: new conception

--- css-reset.js
+++ css-reset.js
@@ -1,30 +1,26 @@
-$(function() {
-
-	function btngrp() {
-		var	btnThis = this,
-			jq_a = $(this).children(),
+var cssReset = {
+	callback: function(el, fn) {
+		el.cssResetCallback = fn;
+		if (!el.cssResetInitialised)
+			this.btngrp(el);
+	},
+	btngrp: function(el) {
+		var	btnThis = el,
+			jq_a = $(el).children(),
 			jq_oldA = jq_a.filter(".css-selected");
+		el.cssResetInitialised = true;
 		jq_a.click(function() {
 			var jq_this = $(this);
-			if (!jq_this.hasClass("css-selected")) {
-				jq_oldA.removeClass("css-selected");
-				jq_this.addClass("css-selected");
-				jq_oldA = jq_this;
-				if (btnThis.cssBtngrpCallback)
-					btnThis.cssBtngrpCallback.call(jq_this);
+			if (jq_this.hasClass("css-selected")) {
+				return false;
 			}
+			jq_oldA.removeClass("css-selected");
+			jq_this.addClass("css-selected");
+			jq_oldA = jq_this;
+			if (btnThis.cssResetCallback)
+				return btnThis.cssResetCallback.call(jq_this);
 		});
 		if (!jq_oldA[0])
 			jq_a.eq(0).click();
 	}
-
-	$(".css-btngrp").each(btngrp);
-
-	$(document.body).on('DOMNodeInserted', function(e) {
-		var jq_target = $(e.target);
-		if (!jq_target.hasClass(".css-btngrp"))
-			jq_target = jq_target.find(".css-btngrp");
-		jq_target.each(btngrp);
-	});
-
-});
+};
